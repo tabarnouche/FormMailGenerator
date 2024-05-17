c721f1ca91e8acc48034181cfb219ff73ede73d6
Tetsuo Arakaki <arakaki@cerulean-net.jp>
2024-05-17 19:02:11 +1200

    style: マークアップとレイアウト調整

--- src/lib/components/input/FormItems.svelte
+++ src/lib/components/input/FormItems.svelte
@@ -5,66 +5,72 @@
 <section class="my-4">
     <h3 class="text-xl my-4">フォーム項目</h3>
     <div class="m-4">
-        <table>
-            <tr>
-                <th class="w-24">id</th>
-                <th>ラベル</th>
-                <th>タイプ</th>
-                <th>必須</th>
-            </tr>
-            <tr>
-                <td>
-                    <input type="text" class="input" value="name" disabled/>
-                </td>
-                <td>
-                    <input type="text" class="input" bind:value={$FormStore.fixedItems.name} />
-                </td>
-                <td>
-                    <select class="select" disabled>
-                        <option>text</option>
-                    </select>
-                </td>
-                <td>
-                    <input type="checkbox" class="checkbox cursor-not-allowed !bg-gray-300 !filter-none" checked disabled/>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="text" class="input" value="email" disabled/>
-                </td>
-                <td>
-                    <input type="text" class="input" bind:value={$FormStore.fixedItems.email} />
-                </td>
-                <td>
-                    <select class="select" disabled>
-                        <option>email</option>
-                    </select>
-                </td>
-                <td>
-                    <input type="checkbox" class="checkbox cursor-not-allowed !bg-gray-300 !filter-none" checked disabled/>
-                </td>
-            </tr>
-            {#each $FormStore.unfixedItems as item, index}
-                <tr>
-                    <td>
-                        <input type="text" class="input" bind:value={item.id} />
-                    </td>
-                    <td>
-                        <input type="text" class="input" bind:value={item.label} />
-                    </td>
-                    <td>
-                        <select class="select" bind:value={item.type}>
-                            <option>text</option>
-                            <option>email</option>
-                            <option>tel</option>
-                            <option>textarea</option>
-                        </select>
-                    </td>
-                    <td>
-                        <input type="checkbox" class="checkbox" bind:checked={$FormStore.unfixedItems[index].required} />
-                    </td>
-                </tr>
-            {/each}
-        </table>
+        <fieldset>
+            <table class="border-separate border-spacing-2">
+                <thead>
+                    <tr>
+                        <th class="w-28">id</th>
+                        <th class="w-44">ラベル</th>
+                        <th>タイプ</th>
+                        <th>必須</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input type="text" class="input" value="name" disabled/>
+                        </td>
+                        <td>
+                            <input type="text" class="input" bind:value={$FormStore.fixedItems.name} />
+                        </td>
+                        <td>
+                            <select class="select" disabled>
+                                <option>text</option>
+                            </select>
+                        </td>
+                        <td>
+                            <input type="checkbox" class="checkbox cursor-not-allowed !bg-gray-300 !filter-none" checked disabled/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="text" class="input" value="email" disabled/>
+                        </td>
+                        <td>
+                            <input type="text" class="input" bind:value={$FormStore.fixedItems.email} />
+                        </td>
+                        <td>
+                            <select class="select" disabled>
+                                <option>email</option>
+                            </select>
+                        </td>
+                        <td>
+                            <input type="checkbox" class="checkbox cursor-not-allowed !bg-gray-300 !filter-none" checked disabled/>
+                        </td>
+                    </tr>
+                    {#each $FormStore.unfixedItems as item, index}
+                        <tr>
+                            <td>
+                                <input type="text" class="input" bind:value={item.id} />
+                            </td>
+                            <td>
+                                <input type="text" class="input" bind:value={item.label} />
+                            </td>
+                            <td>
+                                <select class="select" bind:value={item.type}>
+                                    <option>text</option>
+                                    <option>email</option>
+                                    <option>tel</option>
+                                    <option>textarea</option>
+                                </select>
+                            </td>
+                            <td>
+                                <input type="checkbox" class="checkbox" bind:checked={$FormStore.unfixedItems[index].required} />
+                            </td>
+                        </tr>
+                    {/each}
+                </tbody>
+            </table>
+        </fieldset>
     </div>
 </section>
--- src/lib/components/input/InfoItems.svelte
+++ src/lib/components/input/InfoItems.svelte
@@ -5,45 +5,31 @@
 <section class="my-4">
     <h3 class="text-xl my-4">基本情報</h3>
     <div class="m-4">
-        <table>
-            <tr>
-                <td>
-                    <label class="label" for="companyName">会社・団体名</label>
-                </td>
-                <td>
-                    <input type="text" id="companyName" class="input" bind:value={$InformationStore.companyName} />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label class="label" for="siteName">サイト名</label>
-                </td>
-                <td>
-                    <input type="text" id="siteName" class="input" bind:value={$InformationStore.siteName} />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label class="label" for="emailAddress">メールアドレス</label>
-                </td>
-                <td>
-                    <input type="email" id="emailAddress" class="input" bind:value={$InformationStore.emailAddress} />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label class="label" for="replyLimit">返信期限</label>
-                </td>
-                <td>
-                    <input type="text" id="replyLimit" class="input w-24" bind:value={$InformationStore.replyLimitValue} />
-                    <select class="select w-fit" bind:value={$InformationStore.replyLimitUnit}>
-                        <option>営業日</option>
-                        <option>日</option>
-                        <option>時間</option>
-                    </select>
-                    <span>以内</span>
-                </td>
-            </tr>
-        </table>
+        <fieldset class="grid grid-cols-2 gap-4">
+            <div class="">
+                <label class="label" for="companyName">会社・団体名</label>
+                <input type="text" autocomplete="off" id="companyName" class="input" bind:value={$InformationStore.companyName} />
+            </div>
+            <div class="">
+                <label class="label" for="siteName">サイト名</label>
+                <input type="text" autocomplete="off" id="siteName" class="input" bind:value={$InformationStore.siteName} />
+            </div>
+            <div class="">
+                <label class="label" for="emailAddress">メールアドレス</label>
+                <input type="email" autocomplete="off" id="emailAddress" class="input" bind:value={$InformationStore.emailAddress} />
+            </div>
+            <div class="">
+                <label class="label" for="replyLimit">返信期限</label>
+                <div>
+                  <input type="text" autocomplete="off" id="replyLimit" class="input w-24" bind:value={$InformationStore.replyLimitValue} />
+                  <select class="select w-fit" bind:value={$InformationStore.replyLimitUnit}>
+                      <option>営業日</option>
+                      <option>日</option>
+                      <option>時間</option>
+                  </select>
+                  <span>以内</span>
+                </div>
+            </div>
+        </fieldset>
     </div>
 </section>
